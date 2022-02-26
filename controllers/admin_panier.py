70e706aec4ee5a1df51d88aa3fb94dec499505a5
ObyII <97812645+ObyII@users.noreply.github.com>
2022-02-26 09:53:00 +0100

    Delete controllers directory

--- controllers/admin_panier.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-# -*- coding:utf-8 -*-
-from flask import Blueprint
-from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
-
-from connexion_db import get_db
-
-admin_panier = Blueprint('admin_panier', __name__,
-                        template_folder='templates')
